6319d5cea38ade1dc920f6068ffa9d896191e542
Unknown <shaun@speedfreaks.com.au>
2018-08-02 21:04:36 +0800

    The device name can now be as long as it wants.

--- RadDevice.cpp
+++ RadDevice.cpp
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <RadDevice.h>
+// NOTE: this must be included in main.cpp to compile
 #include <EEPROM.h>
 
 String get_device_id() {
-    // device id is stored at pos 0 and is 6 bytes long.
-    EEPROM.begin(6);
+    // device id is stored at pos 0 and is X bytes long.
+    // As soon as it hits the first NULL char it bails.
+    EEPROM.begin(50);
     String device_id;
-    for (int i=0 ; i < 6 ; i++) {
+
+    for (int i=0 ; i < 50 ; i++) {
         char f = EEPROM.read(i);
+        if (f == NULL) {
+            break;
+        }
         device_id += (f);
     }
     EEPROM.end();
